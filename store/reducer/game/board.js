4d0ed18e2180ce507c5b2d80c654205bedeef611
Pablo Lazo <lazopm@gmail.com>
2018-02-04 20:48:24 -0500

    Better initial state

--- store/reducer/game/board.js
+++ store/reducer/game/board.js
@@ -1,9 +1,23 @@
 import { SET_CELL, NEW_GAME } from '../../actions';
 
+/*
 const createBoardMatrix = () => new Array(9).fill(null)
     .map(() => new Array(9).fill(null));
+*/
 
-const initialState = createBoardMatrix();
+const createBoardMatrix = () => [
+    [null, null, null, null, null,    6, null, null, 3   ],
+    [   6, null,    5,    3,    7, null, null,    8, null],
+    [null, null, null,    5,    1, null, null,    6, null],
+    [null, null,    4,    7, null, null,    8, null, null],
+    [null,    6,    9, null, null, null,    3,    4, null],
+    [null, null,    8, null, null,    5,    2, null, null],
+    [null,    5, null, null,    6,    9, null, null, null],
+    [null,    8, null, null,    3,    1,    7, null,    4],
+    [   2, null, null,    8, null, null, null, null, null],
+];
+
+const initialState = null; 
 export default (state = initialState, { type, ...action }) => {
     switch(type) {
         case(SET_CELL):
